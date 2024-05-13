dc015eb8ec414ca0a476049ae7f91096b0028870
Ester-Arruda <earruda84@gmail.com>
2024-05-13 18:44:44 -0300

    atividade 1

---
+++ TP2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int counterTotal = 0;
+            char caracter;
+            double hTotal = 0, mTotal = 0, cTotal = 0, iTotal = 0;
+            double hPorcentagem = 0, mPorcentagem = 0, cPorcentagem = 0, iPorcentagem = 0;
+
+            while (flag)
+            {
+                Console.WriteLine("H - homem, M - mulher, C - criança, I - idoso, F - finalizar");
+                Console.WriteLine("Entre com um caractere:");
+                caracter = char.Parse(Console.ReadLine());
+                if(caracter == 'F')
+                {
+                    flag = false;
+                }
+                else
+                {
+                    counterTotal += 1;
+
+                    if (caracter == 'H')
+                    {
+                        hTotal += 1;
+                    }
+                    else if (caracter == 'M')
+                    {
+                        mTotal += 1;
+                    }
+                    else if (caracter == 'C')
+                    {
+                        cTotal += 1;
+                    }
+                    else if (caracter == 'I')
+                    {
+                        iTotal += 1;
+                    }
+                }
+            }
+
+            hPorcentagem = hTotal != 0 ? CalculaPorcentagem(counterTotal, hTotal) : 0;
+            mPorcentagem = mTotal != 0 ? CalculaPorcentagem(counterTotal, mTotal) : 0;
+            cPorcentagem = cTotal != 0 ? CalculaPorcentagem(counterTotal, cTotal) : 0;
+            iPorcentagem = iTotal != 0 ? CalculaPorcentagem(counterTotal, iTotal) : 0;
+
+            Console.WriteLine("Total de homens: " + hTotal + " - " + hPorcentagem + "%");
+            Console.WriteLine("Total de mulheres: " + mTotal + " - " + mPorcentagem + "%");
+            Console.WriteLine("Total de crianças: " + cTotal + " - " + cPorcentagem + "%");
+            Console.WriteLine("Total de idosos: " + iTotal + " - " + iPorcentagem + "%");
+        }
+
+        public static double CalculaPorcentagem(double counterTotal, double pessoa)
+        {
+            return (pessoa / counterTotal) * 100;
+        }
+    }
+}
