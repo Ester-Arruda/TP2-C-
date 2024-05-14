6b12479fef237e76cd1a4c78bad7e4d653212fa9
Ester-Arruda <earruda84@gmail.com>
2024-05-14 12:02:48 -0300

    atividade 4

--- TP2/Program.cs
+++ TP2/Program.cs
@@ -90,6 +90,19 @@ namespace TP2
                      Console.WriteLine(i);
                  }
              }
+            int n1 = 0;
+            int n2 = 1;
+            int n3;
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            for (int i = 3; i <= n; i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3);
+                n1 = n2;
+                n2 = n3;
+            }     
         }
         public static double CalculaPorcentagem(double counterTotal, double pessoa)
         {
